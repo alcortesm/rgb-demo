026a5cd7ce3cdb4a4e25fcea104fbf457a392a28
Alberto Cortés <alcortesm@gmail.com>
2015-10-07 03:08:03 +0200

    Add img object (mock)
    A simple img object that poses as a black image.Image of the desired
    size.
    
    The program now prints the img to a file named "rgb.png".

--- rgb-demo.go
+++ rgb-demo.go
@@ -2,8 +2,80 @@
 
 package main
 
-import "os"
+import (
+	"errors"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
 
 func main() {
+	const (
+		side = 400
+		file = "rgb.png"
+	)
+
+	// create an image
+	i, err := newImg(side)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// create an output file
+	// or exit if it already exits
+	f, err := os.OpenFile(file,
+		os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0666)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// write image to the output file
+	if err := png.Encode(f, i); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	os.Exit(0)
+}
+
+// A simple rectangular image.
+// A zero value img is NOT safe, use newImg method as a ctor
+// Implements image.Image.
+type img struct {
+	side int
+}
+
+// Calling this method "new" would be confusing because of the
+// "new()" builtin function, so I have called it "newImg".
+//
+// As I want to return nil imgs on error, I have to return a
+// pointer. For consistency, the rest of the methods also has
+// pointer receivers.
+func newImg(side int) (*img, error) {
+	if side < 1 {
+		return nil, errors.New("image side must be greater than 0")
+	}
+	return &img{side}, nil
+}
+
+// to implement image.Image
+func (i *img) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+// to implement image.Image
+func (i *img) Bounds() image.Rectangle {
+	return image.Rectangle{
+		image.Point{0, 0},
+		image.Point{i.side, i.side},
+	}
+}
+
+// to implement image.Image
+// TODO: add colored circles
+func (i *img) At(x, y int) color.Color {
+	return color.Black
 }
